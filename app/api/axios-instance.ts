bd6b8373e8b677c4a94e1100b199dfce575aa19d
iamroms <54948947+rcenamsi@users.noreply.github.com>
2025-09-22 00:47:19 +0800

    VITE site

--- app/api/axios-instance.ts
+++ app/api/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.SERVER_BASE_URL,
+  baseURL: import.meta.env.VITE_PUBLIC_SITE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
