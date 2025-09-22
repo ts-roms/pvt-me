d43f44d97d9e2b865980249fab16d9a8d3dcea71
iamroms <54948947+rcenamsi@users.noreply.github.com>
2025-09-22 11:34:39 +0800

    Fixes speed insigths

--- app/root.tsx
+++ app/root.tsx
@@ -25,6 +25,9 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: 'preload', as: 'image', href: '/assets/images/pattern.png',
+  }
 ];
 
 export function Layout({children}: Readonly<{ children: React.ReactNode }>) {
