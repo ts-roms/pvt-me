78c698fb81560494a86e834ad2886f58983f7941
iamroms <54948947+rcenamsi@users.noreply.github.com>
2025-10-06 12:08:20 +0800

    Remove banner

--- app/features/home/hero-banner.tsx
+++ app/features/home/hero-banner.tsx
@@ -3,7 +3,6 @@ import {useLoaderData} from "react-router";
 
 export const HeroBanner = () => {
   const {banner} = useLoaderData();
-  console.log({ banner })
   return (
     <section className="relative overflow-hidden py-16 md:py-24">
       <motion.div
